a704d029c565b440ec03ca6241f08390735d751e
psycheguy <yiwenmai2005@gmail.com>
2024-05-22 00:52:37 +0200

    logreg before train

---
+++ log_reg.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report 
+
+df = pd.read_csv("processed_data.csv")
+
+# indicate label columns
+X = df.drop(columns= ['NObeyesdad_Obesity_Type_I','NObeyesdad_Obesity_Type_II'])
+y = df[['NObeyesdad_Obesity_Type_I','NObeyesdad_Obesity_Type_II']]
+
+# split training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state=42)
+
+# initialize and train model
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+
+# evaluate model
+y_pred = logreg.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+
+print(f"Accuracy: {accuracy}")
+print(f"Classification report: {classification_report}")
